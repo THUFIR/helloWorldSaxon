e62853783f577d33d472b47002f4d04077fd427b
thufir <hawat.thufir@gmail.com>
2019-01-04 13:14:39 -0800

    incremental

--- src/main/java/helloWorldSaxon/App.java
+++ src/main/java/helloWorldSaxon/App.java
@@ -1,47 +1,31 @@
 package helloWorldSaxon;
 
 import java.io.IOException;
-import java.io.StringWriter;
+import java.net.URL;
 import java.util.Properties;
 import java.util.logging.Logger;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.stream.StreamResult;
-import org.xml.sax.InputSource;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
 
 public class App {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private final Properties properties = new Properties();
 
-    public static void main(String[] args) throws TransformerException, TransformerConfigurationException, IOException, SAXException {
+    public static void main(String[] args) throws TransformerException, SAXException, ParserConfigurationException, IOException  {
         LOG.fine("starting..");
-        new App().tidySomeWebPage();
+        new App().scrapeHTML();
     }
 
-    private void tidySomeWebPage() throws TransformerConfigurationException, TransformerException, IOException, SAXException {
+    private void scrapeHTML() throws ParserConfigurationException, SAXException, IOException, TransformerException  {
         properties.loadFromXML(App.class.getResourceAsStream("/saxon.xml"));
-        String url = properties.getProperty("mobi");
-
-        StringWriter writer = new StringWriter();
-        StreamResult result = new StreamResult(writer);
-
-        TransformerFactory factory = TransformerFactory.newInstance();
-        XMLReader xmlReader = XMLReaderFactory.createXMLReader("org.ccil.cowan.tagsoup.Parser");
-        Source source = new SAXSource(xmlReader, new InputSource(url));
-
-        Transformer transformer = factory.newTransformer();
-        transformer.transform(source, result);
-        
-        String stringResult = writer.toString();
-        LOG.info(stringResult);
+        URL url = new URL(properties.getProperty("noagenda"));
+        HandlerForXML h = new HandlerForXML(url);
+        String s = h.fetchFromURL();
+        Document d = h.buildDocument(s);
+        LOG.fine(d.toString());
     }
 
 }
---
+++ src/main/java/helloWorldSaxon/HandlerForXML.java
@@ -0,0 +1,69 @@
+package helloWorldSaxon;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.logging.Logger;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+public class HandlerForXML {
+
+    private static final Logger LOG = Logger.getLogger(HandlerForXML.class.getName());
+    private URL url = null;
+
+    private HandlerForXML() {
+    }
+
+    public HandlerForXML(URL url) {
+        this.url = url;
+    }
+
+    public String fetchFromURL() throws TransformerConfigurationException, TransformerException, IOException, SAXException {
+        StringWriter writer = new StringWriter();
+        StreamResult streamResult = new StreamResult(writer);
+
+        TransformerFactory factory = TransformerFactory.newInstance();
+        XMLReader xmlReader = XMLReaderFactory.createXMLReader("org.ccil.cowan.tagsoup.Parser");
+        Source source = new SAXSource(xmlReader, new InputSource(url.toString()));
+
+        Transformer transformer = factory.newTransformer();
+        transformer.transform(source, streamResult);
+
+        String stringResult = writer.toString();
+        LOG.fine(stringResult);
+        return stringResult;
+    }
+
+    public Document buildDocument(String string) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document;
+        document = builder.parse(string);
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory sfactory = SchemaFactory.newInstance(language);
+        Schema schema = sfactory.newSchema(); //????  what is the validation schema
+        Validator validator = schema.newValidator();
+        validator.validate(new DOMSource(document));
+        return document;
+    }
+
+}
