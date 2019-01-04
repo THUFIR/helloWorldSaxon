6cb2da96dda7ce3895ff5ca7341cd54b2d4cc905
thufir <hawat.thufir@gmail.com>
2019-01-04 08:46:15 -0800

    incremental

--- src/main/java/helloWorldSaxon/App.java
+++ src/main/java/helloWorldSaxon/App.java
@@ -1,15 +1,20 @@
 package helloWorldSaxon;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Properties;
 import java.util.logging.Logger;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -20,20 +25,33 @@ public class App {
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private final Properties properties = new Properties();
 
-    public static void main(String[] args) throws TransformerException, TransformerConfigurationException, SAXException, IOException   {
+    public static void main(String[] args) throws TransformerException, TransformerConfigurationException, SAXException, IOException {
         LOG.fine("starting..");
         new App().scrapeBooks();
     }
 
-    private void scrapeBooks() throws TransformerConfigurationException, TransformerException, SAXException, IOException     {
+    private void foo() throws TransformerConfigurationException, TransformerException {
+            StreamSource source = new StreamSource(new StringReader("<xml>blabla</xml>"));
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            Transformer transformer = tFactory.newTransformer();
+            transformer.transform(source, result);
+            String strResult = writer.toString();
+    }
+
+    private void scrapeBooks() throws TransformerConfigurationException, TransformerException, SAXException, IOException {
         properties.loadFromXML(App.class.getResourceAsStream("/saxon.xml"));
         String url = properties.getProperty("url");
 
+        File dir = new File(System.getProperty("user.dir"));
+
         TransformerFactory factory = TransformerFactory.newInstance();
         XMLReader xmlReader = XMLReaderFactory.createXMLReader("org.ccil.cowan.tagsoup.Parser");
-        Source input = new SAXSource(xmlReader,new InputSource(url));
+        Source input = new SAXSource(xmlReader, new InputSource(url));
         Result output = new StreamResult(System.out);
-        factory.newTransformer().transform(input, output);
+        //        StreamResult result = new StreamResult(new File(dir + "\\XML\\" + name + ".xml"));
+        factory.newTransformer().transform(input, (Result) output);
 
     }
 
