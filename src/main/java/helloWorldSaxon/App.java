320653edcb01678e1bd6492c8b16c26ef00ef3b5
thufir <hawat.thufir@gmail.com>
2019-01-05 05:18:29 -0800

    incremental

--- src/main/java/helloWorldSaxon/App.java
+++ src/main/java/helloWorldSaxon/App.java
@@ -1,33 +1,33 @@
 package helloWorldSaxon;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
 import java.util.logging.Logger;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
 
 public class App {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private final Properties properties = new Properties();
 
-    public static void main(String[] args) throws Exception   {
+    public static void main(String[] args) throws Exception {
         LOG.fine("starting..");
         new App().scrapeHTML();
     }
 
-    private void scrapeHTML() throws Exception  {
+    private void scrapeHTML() throws Exception {
         properties.loadFromXML(App.class.getResourceAsStream("/saxon.xml"));
         URL url = new URL(properties.getProperty("books"));
-        //  HandlerForXML handler = new HandlerForXML(url);
         // Document document = handler.createDocumentFromURL();
-        //  handler.printDoc(doc);
         //  handler.writeToFile(document);
 
-        Demo d = new Demo();
-        d.foo();
+        Demo demo = new Demo();
+        // d.createHelloWorldDocument();
+        Document document = demo.createDocumentFromURL(url);
+
+        HandlerForXML handler = new HandlerForXML(url);
+        handler.printDoc(document);
+
     }
 
 }
--- src/main/java/helloWorldSaxon/Demo.java
+++ src/main/java/helloWorldSaxon/Demo.java
@@ -1,24 +1,74 @@
 package helloWorldSaxon;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.net.URL;
+import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 public class Demo {
+
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
 
     public Demo() {
     }
 
-    public void foo() throws Exception {
+    public void writeToFile(Document document) throws Exception {
+        DOMSource source = new DOMSource(document);
+        FileWriter writer = new FileWriter(new File("/tmp/output.xml"));
+        StreamResult result = new StreamResult(writer);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(source, result);
+    }
+
+    public Document createDocumentFromURL(URL url) throws Exception {
+        LOG.fine(url.toString());
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.newDocument();
+
+        Foo foo = new Foo();
+        foo.setBar("Hello World.............................");
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(Foo.class);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.marshal(foo, document);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        XMLReader xmlReader = XMLReaderFactory.createXMLReader("org.ccil.cowan.tagsoup.Parser");
+        Source source = new SAXSource(xmlReader, new InputSource(url.toString()));
+
+        DOMResult domResult = new DOMResult();
+
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(source, domResult);
+
+        //     DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        document = (Document) domResult.getNode();
+
+        return document;
+    }
+
+    public void createHelloWorldDocument() throws Exception {
         // Create the JAXBContext
-        JAXBContext jc = JAXBContext.newInstance(Foo.class);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Foo.class);
 
         // Create the Object
         Foo foo = new Foo();
@@ -30,8 +80,10 @@ public class Demo {
         Document document = db.newDocument();
 
         // Marshal the Object to a Document
-        Marshaller marshaller = jc.createMarshaller();
+        Marshaller marshaller = jaxbContext.createMarshaller();
         marshaller.marshal(foo, document);
+
+        writeToFile(document);
 
         // Output the Document
         TransformerFactory tf = TransformerFactory.newInstance();
